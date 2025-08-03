7fac496408ced4dd7663276346cfaad44a1761b2
ysmk <0620komako@gmail.com>
2025-08-03 22:22:48 +0900

    feat: laravel/breeze をインストール(#1)

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CosmeticController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/cosmetics/create', [CosmeticController::class, 'create'])->name('cosmetics.create');
-Route::post('/cosmetics', [CosmeticController::class, 'store'])->name('cosmetics.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/cosmetics', [CosmeticController::class, 'index'])->name('cosmetics.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
