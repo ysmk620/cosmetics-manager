b4e73a7c28e33d74419b390ed9e8dcc01f3c85a6
ysmk <0620komako@gmail.com>
2025-08-04 11:12:43 +0900

    refactor: ルート定義を認証対応に変更し、ログイン後のリダイレクト先を設定(#1)

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CosmeticController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    }
     return view('welcome');
 });
 
@@ -15,6 +20,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/cosmetics', [CosmeticController::class, 'index'])->name('cosmetics.index');
+    Route::get('/cosmetics/create', [CosmeticController::class, 'create'])->name('cosmetics.create');
+    Route::post('/cosmetics', [CosmeticController::class, 'store'])->name('cosmetics.store');
 });
 
 require __DIR__.'/auth.php';
