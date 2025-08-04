bf99fee5e1f3971646b94fbb85ffe7ba4cdbfba1
ysmk <0620komako@gmail.com>
2025-08-04 09:40:48 +0900

    feat: Cosmeticモデルに user_id を追加し、User–Cosmetic 間のリレーションを設定 (#1)

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function cosmetics()
+    {
+        return $this->hasMany(Cosmetic::class);
+    }
 }
