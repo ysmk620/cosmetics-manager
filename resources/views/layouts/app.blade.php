4070962cfa4345b8863c688d611bfba7ae80ee58
ysmk <0620komako@gmail.com>
2025-08-20 21:56:04 +0900

    refactor: <x-ui.navbar>を採用しメイン構造を整理(#2)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,30 +8,21 @@
 
     <title>{{ config('app.name', 'CosMemo') }}</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
+<body class="antialiased" style="background: var(--bg-app)">
+    <div class="min-h-screen">
+        <x-ui.navbar />
 
-        <!-- Page Heading -->
-        @isset($header)
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
+        <main class="py-10">
+            <div class="container-page">
+                @if (isset($slot))
+                    {{ $slot }}
+                @else
+                    @yield('content')
+                @endif
             </div>
-        </header>
-        @endisset
-
-        <!-- Page Content -->
-        <main>
-            @yield('content')
         </main>
     </div>
     @stack('scripts')
