e71a0722189e9095bae3bc5a43861b85d8fa7207
ysmk <0620komako@gmail.com>
2025-09-09 12:19:14 +0900

    fix; verified ミドルウェアを削除し、ログイン後は確認なしでも遷移可能に(#33)

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route:: middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
